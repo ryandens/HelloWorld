784f43c811feecaf227e89303822635731292719
Ryan Dens <ryan.dens@contrastsecurity.com>
2018-02-17 18:05:29 -0500

    attempt ask init

--- myPhantomTest.js
+++ myPhantomTest.js
@@ -1,10 +1,14 @@
+console.log("\n");
 var system = require('system');
 var env = system.env;
-var REDDIT_PW = "None"
-REDDIT_PW = env["REDDIT_PW"];
+var ASK_EMAIL = "None";
+var ASK_PW = "None";
+ASK_EMAIL = env["ASK_EMAIL"];
+ASK_PW = env["ASK_PW"];
 
 
 var page = new WebPage(), testindex = 0, loadInProgress = false;
+page.settings.userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:44.0) Gecko/20100101 Firefox/44.0";
 
 page.onConsoleMessage = function(msg) {
   console.log(msg);
@@ -23,21 +27,25 @@ page.onLoadFinished = function() {
 var steps = [
   function() {
     //Load Login Page
-    page.open('https://reddit.com');
+    var url = "https://www.amazon.com/ap/signin?_encoding=UTF8&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.pape.max_auth_age=0&ie=UTF8&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_lwa_na&marketPlaceId=ATVPDKIKX0DER&arb=7e19cac3-a482-4fcf-8354-535435361fe8&language=en_US&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fap%2Foa%3FmarketPlaceId%3DATVPDKIKX0DER%26arb%3D7e19cac3-a482-4fcf-8354-535435361fe8%26language%3Den_US&enableGlobalAccountCreation=1&metricIdentifier=amzn1.application.915369ebbc1242368b3179a9cf03c32a&signedMetricIdentifier=ChhIquzflpDeAXDVrIzn02hZNf4hCPinqYBMTmva5xo%3D";
+    page.open(url);
   },
-  function(pw) {
+  function(email, password) {
     //Enter Credentials
-    page.evaluate(function(passwd_input) {
-      var form = document.getElementById('login_login-main');
-      form.elements["user"].value = "dumbtestfortravis";
-      form.elements["passwd"].value = passwd_input;
+    page.evaluate(function(ask_email, ask_password) {
+      var form = document.getElementById('ap_signin_form');
+      console.log("logging in");
+      console.log(ask_email);
+      console.log(ask_password);
+      form.elements["email"].value = ask_email;
+      form.elements["password"].value = ask_password;
       return;
-    }, pw);
+    }, email, password);
   },
   function() {
     //Login
     page.evaluate(function() {
-      var populatedForm = document.getElementById('login_login-main');
+      var populatedForm = document.getElementById('ap_signin_form');
       populatedForm.submit()
       return;
     });
@@ -45,11 +53,16 @@ var steps = [
   function() {
     // Output content of page to stdout after form has been submitted
     page.evaluate(function() {
-      var markup = document.getElementById('header-bottom-right').textContent;
-      console.log(markup);
+      // var markup = document.getElementById('header-bottom-right').textContent;
+      var inner = document.documentElement.innerHTML;
+      var outer = document.documentElement.outerHTML;
+      console.log(inner);
+      console.log("thisismylinedivider");
+      console.log(outer);
     });
   }
 ];
+
 
 
 interval = setInterval(function() {
@@ -58,12 +71,13 @@ interval = setInterval(function() {
       steps[testindex]();
     }
     else {
-      steps[testindex](REDDIT_PW);
+      steps[testindex](ASK_EMAIL, ASK_PW);
     }
     testindex++;
   }
   if (typeof steps[testindex] != "function") {
     // console.log("test complete!");
+    console.log(phantom.cookiesEnabled);
     phantom.exit();
   }
 }, 50);
