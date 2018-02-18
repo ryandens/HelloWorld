336372e03144c891545ac05a53ef6903fff6ded4
Ryan Dens <ryan.dens@contrastsecurity.com>
2018-02-18 15:33:09 -0500

    updated tests and readme

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # HelloWorld
-## An example of how to do continuous integration and deployment with alexa
+An example of how to do continuous integration and deployment with alexa
+
+## Steps
+1. Runs integration tests with bespoken
+2. Authenticates aws and ask
+3. deploys to alexa and lambda
+
 
 [![codecov](https://codecov.io/gh/ryandens/HelloWorld/branch/master/graph/badge.svg)](https://codecov.io/gh/ryandens/HelloWorld)
 
--- test/index-test.js
+++ test/index-test.js
@@ -40,28 +40,28 @@ describe("Test the hello world skill", function() {
             done();
         });
     });
-    
-    // it("Should utter help and get a response", function(done) {
-    //     alexa.utter("help").then((result) => {
-    //         expect(result.response.outputSpeech.ssml).to.exist;
-    //         expect(result.response.outputSpeech.ssml).to.include("This is the Hello World Sample Skill");
-    //         done();
-    //     });
-    // });
-    //
-    // it("Should utter cancel and get a response", function(done) {
-    //     alexa.utter("cancel").then((result) => {
-    //         expect(result.response.outputSpeech.ssml).to.exist;
-    //         expect(result.response.outputSpeech.ssml).to.include("Goodbye!");
-    //         done();
-    //     });
-    // });
-    //
-    // it("Should utter stop and get a response", function(done) {
-    //     alexa.utter("stop").then((result) => {
-    //         expect(result.response.outputSpeech.ssml).to.exist;
-    //         expect(result.response.outputSpeech.ssml).to.include("See you later!");
-    //         done();
-    //     });
-    // });
+
+    it("Should utter help and get a response", function(done) {
+        alexa.utter("help").then((result) => {
+            expect(result.response.outputSpeech.ssml).to.exist;
+            expect(result.response.outputSpeech.ssml).to.include("This is the Hello World Sample Skill");
+            done();
+        });
+    });
+
+    it("Should utter cancel and get a response", function(done) {
+        alexa.utter("cancel").then((result) => {
+            expect(result.response.outputSpeech.ssml).to.exist;
+            expect(result.response.outputSpeech.ssml).to.include("Goodbye!");
+            done();
+        });
+    });
+
+    it("Should utter stop and get a response", function(done) {
+        alexa.utter("stop").then((result) => {
+            expect(result.response.outputSpeech.ssml).to.exist;
+            expect(result.response.outputSpeech.ssml).to.include("See you later!");
+            done();
+        });
+    });
 });
