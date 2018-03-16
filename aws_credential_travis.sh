4793f51fa9426736d719fb5cb0179e837bf3600e
Ryan Dens <ryan.dens@contrastsecurity.com>
2018-03-16 17:32:29 -0400

    fix aws credential bash script

--- aws_credential_travis.sh
+++ aws_credential_travis.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 mkdir -p ~/.aws
+mkdir -p ~/.ask
 
 cat > ~/.aws/credentials << EOL
 [default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOL
+
+
 
 sed -e s/ASK_ACCESS_TOKEN/${ASK_ACCESS_TOKEN}/g -e \
     s/ASK_REFRESH_TOKEN/${ASK_REFRESH_TOKEN}/g sample_ask_config.json > ~/.ask/cli_config
